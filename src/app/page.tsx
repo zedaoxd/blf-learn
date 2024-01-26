d38930cd90bc8c75d0be02ed11d64d01884e7a65
Bruno Lessa <Brunolessa@live.com>
2024-01-26 16:35:55 -0300

    feat: metrics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import ADBanner from "@/components/ad-banner";
 import FeaturedCourses from "@/components/sections/home/featured-courses";
 import Intro from "@/components/sections/home/intro";
+import Metrics from "@/components/sections/home/metrics";
 import TopCategories from "@/components/sections/home/top-categories";
 
 export default function Home() {
@@ -13,6 +14,8 @@ export default function Home() {
       <FeaturedCourses />
 
       <ADBanner />
+
+      <Metrics />
     </main>
   );
 }
